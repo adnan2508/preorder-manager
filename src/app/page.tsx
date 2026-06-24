66461ceb01ac32764ed7f26a0650bcb101103632
adnan2508 <adnandiu2800@gmail.com>
2026-06-24 20:14:28 +0600

    structure added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import "./globals.css";
 export default function Home() {
   return (
     <div>
-      <h2 className="text-red-600">Welcome to the Home Page</h2>
-      <p className="font-extrabold text-3xl">This is the main page of the Pre-Order Manager application.</p>
+      Preorder Manager
     </div>
   );
 }
